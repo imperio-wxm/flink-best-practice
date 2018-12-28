248b9d59b166c31e35923339846a6c5b94c85df6
wxmimperio <wxmimperio@outlook.com>
2018-12-28 16:03:41 +0800

    add checkpoint fs path

--- flink-simple-demo/src/main/java/com/wxmimperio/flink/table/KafkaConnectorTableDsl.java
+++ flink-simple-demo/src/main/java/com/wxmimperio/flink/table/KafkaConnectorTableDsl.java
@@ -3,6 +3,9 @@ package com.wxmimperio.flink.table;
 import org.apache.avro.Schema;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.state.StateBackend;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
@@ -18,7 +21,9 @@ public class KafkaConnectorTableDsl {
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StateBackend stateBackend = new FsStateBackend("file:///D:/d_backup/github/flink-best-practice/checkpoint");
         env.enableCheckpointing(1000 * 60);
+        env.setStateBackend(stateBackend);
 
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "10.174.20.33:9092");
@@ -48,14 +53,24 @@ public class KafkaConnectorTableDsl {
                 new Avro().avroSchema(schenaStr)
         ).inAppendMode().registerTableSource("rtc_warning_gmys");
 
-        Table table = tableEnv.scan("rtc_warning_gmys");
-        table.select("character_id");
+        // scan table
+        //Table table = tableEnv.scan("rtc_warning_gmys");
 
-        DataStream<Row> dsRow = tableEnv.toAppendStream(table, Row.class);
-        dsRow.map(new MapFunction<Row, Object>() {
+        // query table
+        Table table = tableEnv.sqlQuery("select sum(area_id) from rtc_warning_gmys where area_id = 1 group by character_id,area_id,group_id,platform");
+
+        // convert the Table into a retract DataStream of Row.
+        //   A retract stream of type X is a DataStream<Tuple2<Boolean, X>>.
+        //   The boolean field indicates the type of the change.
+        //   True is INSERT, false is DELETE.
+        DataStream<Tuple2<Boolean, Row>> dsRow = tableEnv.toRetractStream(table, Row.class);
+        dsRow.map(new MapFunction<Tuple2<Boolean,Row>, Object>() {
             @Override
-            public Object map(Row row) throws Exception {
-                System.out.println(row.toString());
+            public Object map(Tuple2<Boolean, Row> booleanRowTuple2) throws Exception {
+                if(booleanRowTuple2.f0) {
+                    System.out.println(booleanRowTuple2.f1.toString());
+                    return booleanRowTuple2.f1;
+                }
                 return null;
             }
         });
