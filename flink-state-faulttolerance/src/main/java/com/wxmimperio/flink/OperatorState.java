47d5038500992c1a3ca1970f2bd189b4eef5e73f
imperio-wxm <wxmimperio@outlook.com>
2020-05-20 11:44:18 +0800

    update: OperatorState

--- flink-state-faulttolerance/src/main/java/com/wxmimperio/flink/OperatorState.java
+++ flink-state-faulttolerance/src/main/java/com/wxmimperio/flink/OperatorState.java
@@ -1,8 +1,95 @@
 package com.wxmimperio.flink;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OperatorState {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("10.1.8.210", 9999)
+                .flatMap(new Splitter())
+                .keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .sum(1);
+
+        dataStream.print();
+        dataStream.addSink(new BufferingSink(2));
+
+        env.execute("Window WordCount");
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for (String word : sentence.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+    }
+
+    // ListCheckpointed
+    public static class BufferingSink implements SinkFunction<Tuple2<String, Integer>>, CheckpointedFunction {
+        private final int threshold;
+        private transient ListState<Tuple2<String, Integer>> checkPointState;
+        private List<Tuple2<String, Integer>> bufferedElements;
+
+        public BufferingSink(int threshold) {
+            this.threshold = threshold;
+            this.bufferedElements = new ArrayList<>();
+        }
+
+        @Override
+        public void invoke(Tuple2<String, Integer> value, Context context) throws Exception {
+            bufferedElements.add(value);
+            if (bufferedElements.size() == threshold) {
+                for (Tuple2<String, Integer> element : bufferedElements) {
+                    System.out.println("send = " + element);
+                    // send to sink
+                }
+                bufferedElements.clear();
+            }
+        }
+
+        @Override
+        public void snapshotState(FunctionSnapshotContext functionSnapshotContext) throws Exception {
+            checkPointState.clear();
+            for (Tuple2<String, Integer> element : bufferedElements) {
+                checkPointState.add(element);
+            }
+        }
+
+        @Override
+        public void initializeState(FunctionInitializationContext functionInitializationContext) throws Exception {
+            ListStateDescriptor<Tuple2<String, Integer>> descriptor = new ListStateDescriptor<Tuple2<String, Integer>>(
+                    "buffered-elements",
+                    TypeInformation.of(new TypeHint<Tuple2<String, Integer>>() {
+                    })
+            );
+            checkPointState = functionInitializationContext.getOperatorStateStore().getListState(descriptor);
+            // Use the isRestored() method of the context to check if we are recovering after a failure.
+            if (functionInitializationContext.isRestored()) {
+                for (Tuple2<String, Integer> element : checkPointState.get()) {
+                    bufferedElements.add(element);
+                }
+            }
+        }
     }
 }
