73df9f508f0b0b62a5ebd59bcb3e6f6478f6079a
weiximing.imperio <wxmimperio@outlook.com>
2020-05-21 16:58:44 +0800

    update: CheckPoint

--- flink-state-faulttolerance/src/main/java/com/wxmimperio/flink/CheckPoint.java
+++ flink-state-faulttolerance/src/main/java/com/wxmimperio/flink/CheckPoint.java
@@ -1,6 +1,7 @@
 package com.wxmimperio.flink;
 
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
@@ -8,6 +9,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 
 public class CheckPoint {
+
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
@@ -23,9 +25,27 @@ public class CheckPoint {
         // 同一时间只允许一个 checkpoint 进行
         env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
         // 开启在 job 中止后仍然保留的 externalized checkpoints
+        // Checkpoint 在默认的情况下仅用于恢复失败的作业，并不保留，当程序取消时 checkpoint 就会被删除。
+        // 当然，你可以通过配置来保留 checkpoint，这些被保留的 checkpoint 在作业失败或取消时不会被清除
+        // ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION：当作业取消时，保留作业的 checkpoint。注意，这种情况下，需要手动清除该作业保留的 checkpoint。
+        // ExternalizedCheckpointCleanup.DELETE_ON_CANCELLATION：当作业取消时，删除作业的 checkpoint。仅当作业失败时，作业的 checkpoint 才会被保留
         env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
         // 允许在有更近 savepoint 时回退到 checkpoint
         env.getCheckpointConfig().setPreferCheckpointForRecovery(true);
+
+        String path = "";
+        // 1. MemoryStateBackend（default）
+        //  在 MemoryStateBackend 内部，数据以 Java 对象的形式存储在堆中。 Key/value 形式的状态和窗口算子持有存储着状态值、触发器的 hash table。
+        //  在 CheckPoint 时，State Backend 对状态进行快照，并将快照信息作为 CheckPoint 应答消息的一部分发送给 JobManager(master)，同时 JobManager 也将快照信息存储在堆内存中
+
+        // 2. FsStateBackend
+        //  FsStateBackend 将正在运行中的状态数据保存在 TaskManager 的内存中。CheckPoint 时，将状态快照写入到配置的文件系统目录中。 少量的元数据信息存储到 JobManager 的内存中
+
+        // 3. RocksDBStateBackend
+        //  RocksDBStateBackend 将正在运行中的状态数据保存在 RocksDB 数据库中，RocksDB 数据库默认将数据存储在 TaskManager 的数据目录。
+        //  CheckPoint 时，整个 RocksDB 数据库被 checkpoint 到配置的文件系统目录中。 少量的元数据信息存储到 JobManager 的内存中
+        //  RocksDBStateBackend 只支持异步快照，支持增量快照
+        env.setStateBackend(new FsStateBackend(path, false));
 
         DataStream<Tuple2<String, Integer>> dataStream = env
                 .socketTextStream("10.1.8.210", 9999)
