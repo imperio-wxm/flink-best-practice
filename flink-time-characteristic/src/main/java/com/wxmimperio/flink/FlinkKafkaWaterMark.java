c782ba136a08c59009838e278bfed13ebac06851
wxmimperio <wxmimperio@outlook.com>
2020-10-15 15:01:18 +0800

    update version to 1.11.2

--- flink-time-characteristic/src/main/java/com/wxmimperio/flink/FlinkKafkaWaterMark.java
+++ flink-time-characteristic/src/main/java/com/wxmimperio/flink/FlinkKafkaWaterMark.java
@@ -2,6 +2,7 @@ package com.wxmimperio.flink;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.wxmimperio.flink.extractor.MyExtractor;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
